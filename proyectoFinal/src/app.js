8f82223c80eb485db739351f91ec06439638a722
Hugo Galbarini <hgalbarini@gmail.com>
2026-01-27 15:57:06 -0300

    get product with gitignore

--- proyectoFinal/src/app.js
+++ proyectoFinal/src/app.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express from "express";
 /* import CartManager from "./CartManager.js";*/
 import Product from "./Product.js";
 import ProductManager from "./ProductManager.js"; 
